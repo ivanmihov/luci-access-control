c9ee106faeb98ae19f9d5953c08887358820aba0
Ivan Mihov <ivanmihov@users.noreply.github.com>
2018-02-19 20:57:01 -0800

    Change protocol from 0 to all as is on wrt1200

--- luci-app-access-control/luasrc/model/cbi/access_control.lua
+++ luci-app-access-control/luasrc/model/cbi/access_control.lua
@@ -67,7 +67,7 @@ local s_rule = mr:section(TypedSection, "rule", translate("Client Rules"))
     s_rule.defaults.src     = "*" --"lan", "guest" or enything on local side
     s_rule.defaults.dest    = "wan"
     s_rule.defaults.target  = "REJECT"
-    s_rule.defaults.proto    = "0"
+    s_rule.defaults.proto    = "all"
     s_rule.defaults.extra = "--kerneltz"
     
     -- only AC-related rules
@@ -107,7 +107,7 @@ local s_rule = mr:section(TypedSection, "rule", translate("Client Rules"))
 --            self.map:set(section, "src",  "*")
 --            self.map:set(section, "dest", "wan")
 --            self.map:set(section, "target", "REJECT")
---            self.map:set(section, "proto", "0")
+--            self.map:set(section, "proto", "all")
 --            self.map:set(section, "extra", "--kerneltz")
             return Flag.write(self, section, value)
         end
